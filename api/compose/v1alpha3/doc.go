d4db3a328402ae065e7567d3893ff4fbaf048f65
Simon Ferquel <simon.ferquel@docker.com>
2018-12-19 10:34:34 +0100

    v1alpha3 types
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

---
+++ api/compose/v1alpha3/doc.go
@@ -0,0 +1,7 @@
+// Api versions allow the api contract for a resource to be changed while keeping
+// backward compatibility by support multiple concurrent versions
+// of the same resource
+
+// Package v1alpha3 is the current in dev version of the stack, containing evolution on top of v1beta2 structured spec
+// +k8s:openapi-gen=true
+package v1alpha3
